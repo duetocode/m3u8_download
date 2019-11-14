aa0a5e24dfa87b964d7c11167279215ad21cf822
Liang <elvewyn@gmail.com>
2019-11-14 14:45:50 +0800

    Improve file sorting

--- concatenate.sh
+++ concatenate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Concatenating all pieces...'
-cat $(ls -1 ts/*.ts | sort -n ) > all.ts
+cat $(ls -1 ts/*.ts | sort -V ) > all.ts
 
 echo 'Repackaging...'
 ffmpeg -i all.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc $1
